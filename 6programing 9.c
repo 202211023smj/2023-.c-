586e2eb90f93174062b9b55cd7509da0031ed775
202211023smj <121844264+202211023smj@users.noreply.github.com>
2023-04-14 15:51:35 +0900

    Add files via upload

---
+++ 6programing 9.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) 
+{
+	float x, value;
+
+	printf("x의 값을 입력하시오: ");
+	scanf("%f", &x);
+
+	if (x <= 0)
+		value = x * x - 9 * x + 2;
+	else
+		value = 7 * x + 2;
+
+	printf("f(x)의 값은 %f\n", value);
+
+	return 0;
+}

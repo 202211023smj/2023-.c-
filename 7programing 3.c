c0e3fb5f556975c50d3ebd21b2a8436914a234da
202211023smj <121844264+202211023smj@users.noreply.github.com>
2023-04-21 17:18:15 +0900

    Add files via upload

---
+++ 7programing 3.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int n;
+	printf("정수를 입력하시오:");
+	scanf("%d", &n);
+	
+	for(int i=1;i<=n;i++)
+	{
+		if(n%1==0)
+		{
+			printf("%d\n", i);
+		}
+	}
+	return 0;
+}

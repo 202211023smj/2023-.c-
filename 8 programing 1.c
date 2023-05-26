ed0ec62300f3146b62289b467caa001b3e6d1ee4
202211023smj <121844264+202211023smj@users.noreply.github.com>
2023-05-27 00:38:32 +0900

    Add files via upload

---
+++ 8 programing 1.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+double square(double num) {
+	return num * num;
+}
+int main(void) {
+	double n;
+	printf("정수를 입력하세요: ");
+	scanf("%lf", &n);
+	printf("주어진 정수 %f의 제곱은 %f입니다", n, square(n));
+	return 0;
+}
